a3ac41e1b9da070a1e7bc1111e5f9408f856a375
Edmund Ekott <edmund.timfon@gmail.com>
2021-07-14 19:07:48 +0100

    cleanup

--- state/Cart.js
+++ state/Cart.js
@@ -10,17 +10,12 @@ export const useGlobalState = () => {
   return {
     get: () => cart.value,
     addToCart: (product) => {
-      // cart.products['item-1'].quantity.value;
       if (cart.products[`item-${product.id}`].value) {
         cart.products[`item-${product.id}`].merge({ quantity: cart.products[`item-${product.id}`].quantity.value + 1 });
         cart.size.set(cart.size.value + 1);
-        // console.log(cart.products);
-        // cart.products.merge({ [`item-${product.id}`]: { ...product, quantity: cart.products[`item-${product.id}`].quantity + 1 } });
-        // cart.merge({ size: cart.size.value + 1 });
       } else {
         cart.products.merge({ [`item-${product.id}`]: { ...product, quantity: 1 } });
         cart.size.set(cart.size.value + 1);
-        // cart.merge({ size: cart.size.value + 1 });
       }
     },
   };
